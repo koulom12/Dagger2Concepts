3b35e761d53f6fca7db709fdee6b9a3de4fca601
omeshkoul <koulom12@gmail.com>
2023-05-06 19:20:40 +0530

    Qualifier Annotations

--- app/src/main/java/com/example/dagger2concepts/NotificationServiceModule.kt
+++ app/src/main/java/com/example/dagger2concepts/NotificationServiceModule.kt
@@ -2,12 +2,20 @@ package com.example.dagger2concepts
 
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class NotificationServiceModule {
 
+    @MessageQualifier
     @Provides
     fun getMessageService() : NotificationService {
         return MessageService()
+    }
+
+    @Named("email")
+    @Provides
+    fun getEmailService(emailService: EmailService) : NotificationService {
+        return emailService
     }
 }
--- app/src/main/java/com/example/dagger2concepts/UserRegistrationService.kt
+++ app/src/main/java/com/example/dagger2concepts/UserRegistrationService.kt
@@ -4,7 +4,7 @@ import javax.inject.Inject
 
 
 class UserRegistrationService @Inject constructor(private val userRepository: UserRepository,
-                              private val notificationService: NotificationService
+                              @MessageQualifier private val notificationService: NotificationService
 ) {
     fun registerUser(email: String, password: String) {
         userRepository.saveUser(email, password)
